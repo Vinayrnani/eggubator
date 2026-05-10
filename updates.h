aaa1373df8ee00a341b863eee74703b1e6f62365
root <root@localhost.localdomain>
2026-05-10 13:32:31 +0000

    Bump version to 1.3.10

--- updates.h
+++ updates.h
@@ -8,7 +8,7 @@
 
 #define FIRMWARE_URL "http://YOUR_SERVER/firmware.bin"
 #define VERSION_URL "http://YOUR_SERVER/version.txt"
-#define FIRMWARE_VERSION "1.3.9"
+#define FIRMWARE_VERSION "1.3.10"
 
 extern ESP8266HTTPUpdateServer httpUpdater;
 
