09e776d747116ff3be1a1252bde9e93386e8eba1
root <root@localhost.localdomain>
2026-05-26 08:18:36 +0000

    bump 1.3.33
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- updates.h
+++ updates.h
@@ -8,7 +8,7 @@
 
 #define FIRMWARE_URL "http://YOUR_SERVER/firmware.bin"
 #define VERSION_URL "http://YOUR_SERVER/version.txt"
-#define FIRMWARE_VERSION "1.3.32"
+#define FIRMWARE_VERSION "1.3.33"
 
 extern ESP8266HTTPUpdateServer httpUpdater;
 
