ff8f13b282fdb6788b69e9b26103692455ed0373
root <root@localhost.localdomain>
2026-05-28 15:02:34 +0000

    release: v1.4.6

--- config.h
+++ config.h
@@ -28,6 +28,6 @@ extern unsigned long EGG_TURN_INTERVAL;
 extern unsigned long EGG_TURN_DURATION;
 
 // Firmware Version
-#define FIRMWARE_VERSION "1.4.5"
+#define FIRMWARE_VERSION "1.4.6"
 
 #endif
