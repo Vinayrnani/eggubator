ac5962e2a8d8a67b59cc524f48f0a2feb4695efc
root <root@localhost.localdomain>
2026-05-28 16:22:34 +0000

    release: v1.4.8

--- updates.h
+++ updates.h
@@ -10,7 +10,7 @@
 
 #define FIRMWARE_URL "https://github.com/Vinayrnani/eggubator/releases/latest/download/firmware.bin"
 #define VERSION_URL "https://api.github.com/repos/Vinayrnani/eggubator/releases/latest"
-#define FIRMWARE_VERSION "1.4.3"
+#define FIRMWARE_VERSION "1.4.8"
 
 extern ESP8266HTTPUpdateServer httpUpdater;
 
